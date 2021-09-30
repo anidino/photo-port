bde563fae28e13282fa9cb647529e90abfef8ad4
AniElaina <ani.elaina.carr@gmail.com>
2021-09-30 17:52:12 -0400

    saving changes post lssn4

--- src/components/About/__tests__/index.test.js
+++ src/components/About/__tests__/index.test.js
@@ -1,28 +1,19 @@
+// __tests__/About.test.js
 import React from 'react';
-
 import { render, cleanup } from '@testing-library/react';
-import '@testing-library/jest-dom/extend-expect'; 
+import '@testing-library/jest-dom/extend-expect';
+import About from '..';
 
-import About from '../../About';
-
-// ensures that after each test, we won't have any leftover memory data that could give us false results
 afterEach(cleanup);
 
-//declare the component being tested 
-//the first argument is the string that will be tested. the second is a callbACK function that runs it 
-describe('About component', () => {
-	//first test 
-	it('renders', () => {
-		render(<About />);
-	});
-
-	//second test 
-	it('matches snapshot DOM node structure', () => {
-		//render About 
-		const { asFragment } = render(<About />);
-
-		expect(asFragment()).toMatchSnapshot();
-	})
-
-
+describe('About component renders', () => {
+  it('renders', () => {
+    render(<About />);
+  });
+  
+  it('matches snapshot DOM node structure', () => {
+    const { asFragment } = render(<About />);
+    
+    expect(asFragment()).toMatchSnapshot();
+  });
 })
